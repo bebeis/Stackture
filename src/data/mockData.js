b534d34854c571b69ba5d1e0df26185645c8945f
DawnGlow <ferrater1013@gmail.com>
2024-11-25 23:51:09 +0900

    change mockdata location

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -2,31 +2,31 @@ export const mockTechStacks = [
     {
         id: 1,
         name: 'React',
-        icon: '/public/icons/react.svg',
+        icon: './public/icons/react.svg',
         category: 'frontend'
     },
     {
         id: 2,
         name: 'Node.js',
-        icon: '/public/icons/nodejs.svg',
+        icon: './public/icons/nodejs.svg',
         category: 'backend'
     },
     {
         id: 3,
         name: 'MongoDB',
-        icon: '/public/icons/mongodb.svg',
+        icon: './public/icons/mongodb.svg',
         category: 'database'
     },
     {
         id: 4,
         name: 'Docker',
-        icon: '/public/icons/docker.svg',
+        icon: './public/icons/docker.svg',
         category: 'devops'
     },
     {
         id: 5,
         name: 'Python',
-        icon: '/public/icons/python.svg',
+        icon: './public/icons/python.svg',
         category: 'language'
     }
 ];
